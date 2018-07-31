290036ca9974fe10bfca06b81dea90bc784f8ccf
Erin Penner <epenner0522@gmail.com>
2018-07-31 18:12:21 +0000

    automatically backed up by learn

---
+++ loops.js
@@ -0,0 +1,29 @@
+function forLoop(array) {
+  for (var i=0; i < 25; i++) {
+    array.push(`"I am ${i+1} strange loop${i === 0 ? '' : 's'}."`);
+  }
+  return array;
+}
+
+function whileLoop(n) {
+  while (n > 0) {
+    console.log(n);
+    n--;
+  }
+  
+  return 'done';
+}
+
+function doWhileLoop(array) {
+  
+}
+
+
+var array = [];
+array = forLoop(array);
+console.log(array);
+
+var n=3;
+var resp;
+resp = whileLoop(n);
+console.log(resp);
